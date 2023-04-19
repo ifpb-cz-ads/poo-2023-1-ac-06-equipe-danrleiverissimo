3130fc8fb974fd3133170311dc589de692371109
Veríssimo Terceiro <terceiroverissimo@gmail.com>
2023-04-19 08:29:11 -0300

    Update Lampada.java

--- Lampada.java
+++ Lampada.java
@@ -1,32 +1,22 @@
 public class Lampada {
     private String marca;
     private double preco;
-    private boolean ligada;
+    private int estoque;
 
     //setters e getters iriam aqui. Omiti por simplicidade
 
-    public void ligar() {
-        if (!ligada) {
-            System.out.println("Ligando a lâmpada.");
-            ligada = true;
-        } else {
-            System.out.println("A lâmpada já está ligada.");
-        }
+    public void addEstoque(int estoque) {
+        this.estoque += estoque;
     }
 
-    public void desligar() {
-        if (ligada) {
-            System.out.println("Desligando a lâmpada.");
-            ligada = false;
-        } else {
-            System.out.println("A lâmpada já está desligada.");
-        }
+    public void rmEstoque(int estoque) {
+        this.estoque -= estoque;
     }
 
     public void mostrarDados() {
         System.out.println("Marca: " + marca);
         System.out.println("Preço: R$" + preco);
-        System.out.println("Status: " + (ligada ? "Ligada" : "Desligada"));
+        System.out.println("Estoque: " + estoque + "unidades");
     }
 
 
