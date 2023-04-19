92d34b41c133811da42b6b3eefd42a3f0923eda7
Veríssimo Terceiro <terceiroverissimo@gmail.com>
2023-04-19 08:29:31 -0300

    Update Time.java

--- Time.java
+++ Time.java
@@ -1,6 +1,8 @@
 public class Time {
     private String nome;
     private int pontos;
+    public int colocacao;
+    //suponho que a lógica para alterar a colocação do time seja feita em outra classe
 
     //setters e getters iriam aqui. Omiti por simplicidade
 
@@ -11,6 +13,7 @@ public class Time {
     void mostrarDados(){
         System.out.println("Nome do time: " + nome);
         System.out.println("Pontuação: " + pontos + " pontos");
+        System.out.println("Colocação atual: " + colocacao);
     }
 }
 
